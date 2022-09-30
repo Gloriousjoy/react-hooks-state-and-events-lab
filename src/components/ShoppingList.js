3b41a76d9dac77e9cb0ea59b9985d631f8fd3863
Gloriousjoy <gloriousfab99@gmail.com>
2022-10-01 01:54:33 +0300

    completed assignment

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -15,7 +15,7 @@ function ShoppingList({items}) {
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter" onClick={handleCategoryChange}>
+        <select name="filter" onChange={handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
